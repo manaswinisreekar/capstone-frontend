c8c29b6ee00aa74fd1bd00d01f0b0e685bcb7cc6
manaswinisreekar <manaswinikolluru@gmail.com>
2020-11-16 12:38:29 +0530

    Validation and Integratiion added

---
+++ src/screens/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return(
+            <Header showSearchBox={true}/>
+        
+        )
+    }
+}
+
+export default Home; 
